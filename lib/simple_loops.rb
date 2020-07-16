95bf8fc97ec9abb69835f6c0c842834edd8915a7
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-16 19:30:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,19 +1,18 @@
 def loop_message_five_times(message)
   count = 0
-  message = "Hello World."
   while count < 5 do
     puts message
     count += 1
   end
 end
 
-def loop_message_n_times(message, number)
+def loop_message_n_times(message, limit)
   count = 0
-  while count < number do
-    puts "#{message}"
+  while count < limit do
+    puts message
     count += 1
   end
-  end
+end
 
 def output_array(array)
   count = 0
@@ -21,4 +20,8 @@ def output_array(array)
     puts array[count]
     count += 1
   end
-  end
+end
+
+def return_string_array(array)
+  puts array.each{|i|}
+end  
